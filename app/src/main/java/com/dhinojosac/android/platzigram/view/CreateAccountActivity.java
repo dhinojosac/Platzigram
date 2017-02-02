84f7a252d82e2bcdeb463be1196a7efa6f7adda4
dhinojosac <d.hinojosac.cordova@gmail.com>
2017-02-02 08:33:00 -0300

    cardview layout and Create account view are added

--- app/src/main/java/com/dhinojosac/android/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/dhinojosac/android/platzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.dhinojosac.android.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.dhinojosac.android.platzigram.R;
 
@@ -11,5 +12,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.createaccount_toolbar_text), true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
